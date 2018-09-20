6806495b024ea01ab045d3bfaa94fe22dccccaa0
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2018-09-20 15:10:29 -0500

    Update ejercicio3.java

--- ejercicio3.java
+++ ejercicio3.java
@@ -1,56 +1,72 @@
 import java.io.*;
 
 /**
- * 
- * @author Andrés Guillermo Bonilla Olarte
  *
+ * @author Andrés Guillermo Bonilla Olarte
  */
-public class ejercicio3
+
+public class ejercicio3 
 {
-	
-/**
- * 
- * @param args
- */
-    public static void main(String[] args) 
+        static BufferedReader br = new BufferedReader (new InputStreamReader (System.in));       
+        static BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out)); 
+         /**
+         * 
+         * @param total
+         * @param sub
+         * @return 
+         */
+    public static int substring_occurrences(String total, String sub)
     {
-        BufferedReader br = new BufferedReader ( new InputStreamReader ( System.in ));
-        BufferedWriter bw = new BufferedWriter ( new OutputStreamWriter ( System.out ));
-        
-        
-        try
-        {   
-        	String string;   
-        	char [] Arraystring;
-        	char charact;
-        	int accountant = 0;
-        	bw.write("Write your string ");
-        	bw.newLine();
-        	bw.flush();
-        	string = br.readLine();
-        	bw.newLine();
-        	Arraystring = string.toCharArray();
-        	for (int h = 0;h < Arraystring.length; h++) 
-        	{
-        		charact = Arraystring[h];
-        		for (int j = 0; j < Arraystring.length; j++) 
-        		{
-        			if (Arraystring [j] == charact) 
-        			{
-        				accountant++;
-        			}
-        		}
-        		bw.write(Arraystring[h]+" "+accountant);
-        		bw.newLine();
-        		accountant = 0;
-        		bw.flush();
-        	}
-        	
-        	bw.flush(); //libera información
-        }
-        catch(Exception ex)
+        int counter = 0;
+        for (int i = 0; i <= (total.length() - sub.length()); i++)
         {
-        	ex.printStackTrace();
+            if (total.substring(i, i + sub.length()).equals(sub))
+            {
+                counter += 1; 
+            }
+        }
+        return counter;
+    }
+     /**
+     * 
+     * @param total
+     * @throws IOException 
+     */
+    public static void all_substrings (String total) throws IOException
+    {
+        int max = Integer.MIN_VALUE;
+        String winner = "";
+        String temp = "";
+        
+         for (int i = 2; i <= total.length(); i++)
+        {
+            for (int j = 0; j <= (total.length() - i); j++)
+            {
+                if (substring_occurrences(total, total.substring( j, j+i )) > max)
+                {
+                    max = substring_occurrences(total, total.substring( j, j+i ));
+                    winner = total.substring(j, j+i);
+                }
+            }        
+        }      
+        bw.write("La cadena que más se repite es "+ winner+ " y se repite "+max+ " veces\n");
+        bw.flush();
+    }
+	/**
+      * 
+      * @param args 
+      */
+    public static void main (String [] args)
+    {
+
+        String input;
+        try
+        {
+            input = br.readLine();
+            all_substrings(input);
+        }
+        catch (Exception ex)
+        {
         }
     }
-} 
+}
