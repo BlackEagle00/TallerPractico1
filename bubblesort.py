6f03f93284bcbbd624c684c50a4fc7d31c2ccd38
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2018-10-18 15:03:51 -0500

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -13,7 +13,29 @@ def bubble_sort(a):
         if not swapped:
             break
 
-a = [randint (1, 1000) for i in range (20)]
+def insertion_sort(a):
+    for i in range (1 , len(a)):
+        x = a[i]
+        index_hole = i
+        while index_hole > 0 and a [index_hole - 1] > x:
+            a [index_hole] = a[index_hole - 1]
+            index_hole -= 1
+        a[index_hole] = x    
+
+def quick_sort(a):
+    if len(a) == 1:
+        return a
+    else:
+        pivot = a[0]
+        for i in range (len(a) ):
+            if a[i] < pivot:
+                less_subarray = a[i]
+            else:
+                greater_subarray = a[i]    
+            return quick_sort(less_subarray) + pivot + quick_sort(greater_subarray)           
+    
+
+a = [randint (1, 1000) for i in range (10)]
 print (a)
-bubble_sort(a)
-print (a)       
+quick_sort(a)
+print (a)
